b8d8e8e349a1d33dc1a120fa495b07ecd514c6df
wojcho <wojhoo3@gmail.com>
2026-06-16 05:58:03 +0200

    Initialize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section id="center">
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+    </>
+  );
+}
+
+export default App;
